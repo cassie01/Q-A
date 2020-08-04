587b55d43eaf4317dd8ad8f1e5957fa20589174d
Xiang <Cassie.Xiang@smiths-medical.com>
2020-08-04 16:44:13 +0800

    一次小练习

--- conftest.py
+++ conftest.py
@@ -10,22 +10,22 @@ def foo():
     yield 100
     print('function end')
 
-# @pytest.hookimpl(hookwrapper=True, tryfirst=True)
-# def pytest_runtest_makereport(item, call):
-#     print('------------------------------------')
-#
-#     # 获取钩子方法的调用结果
-#     out = yield
-#     print('用例执行结果', out)
-#
-#     # 3. 从钩子方法的调用结果中获取测试报告
-#     report = out.get_result()
-#
-#     print('测试报告：%s' % report)
-#     print('步骤：%s' % report.when)
-#     print('nodeid：%s' % report.nodeid)
-#     print('description:%s' % str(item.function.__doc__))
-#     print(('运行结果: %s' % report.outcome))
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    print('------------------------------------')
+
+    # 获取钩子方法的调用结果
+    out = yield
+    print('用例执行结果', out)
+
+    # 3. 从钩子方法的调用结果中获取测试报告
+    report = out.get_result()
+
+    print('测试报告：%s' % report)
+    print('步骤：%s' % report.when)
+    print('nodeid：%s' % report.nodeid)
+    print('description:%s' % str(item.function.__doc__))
+    print(('运行结果: %s' % report.outcome))
 
 def pytest_sessionstart(session):
     session.results = dict()
